ee5ed389d11f6723cf13d18d1d62fc7761f73c3c
sidd-92 <sidddharthvn2014@gmail.com>
2018-10-29 20:36:36 -0700

    Refactor Data

--- quoteswithtag/src/App.js
+++ quoteswithtag/src/App.js
@@ -2,30 +2,32 @@ import React, { Component } from "react";
 import Quote from "./Components/Quote/Quote";
 import "./App.css";
 import {
-  getAllQuotes,
-  getAllQuotesWithAuthor,
-  getTagsForQuote
+	getAllQuotes,
+	getAllQuotesWithAuthor,
+	getTagsForQuote,
+	getAllTags
 } from "./Services/quotesDatabase.js";
 class App extends Component {
-  render() {
-    const quote = getAllQuotes();
-    const allQ = getAllQuotesWithAuthor();
-    const tag = getTagsForQuote();
+	render() {
+		getAllTags();
+		const quote = getAllQuotes();
+		const allQ = getAllQuotesWithAuthor();
+		const tag = getTagsForQuote();
 
-    return (
-      <div className="container">
-        <h1>List of Quotes</h1>
-        {quote.map(quote => (
-          <Quote
-            key={quote}
-            quote={quote}
-            author={allQ[0][quote]}
-            tag={tag[0][quote]}
-          />
-        ))}
-      </div>
-    );
-  }
+		return (
+			<div className="container">
+				<h1>List of Quotes</h1>
+				{quote.map(quote => (
+					<Quote
+						key={quote}
+						quote={quote}
+						author={allQ[0][quote]}
+						tag={tag[0][quote]}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
