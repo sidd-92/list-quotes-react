4329bbb61241c0181779e56519bf35654ff0bd4d
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2019-10-07 17:08:31 +0530

    Fixed

--- quoteswithtag/src/App.js
+++ quoteswithtag/src/App.js
@@ -2,26 +2,26 @@ import React, { Component } from "react";
 import Quote from "./Components/Quote/Quote";
 import "./App.css";
 import {
-	getInformationByQuote,
-	getAllQuotes
+  getInformationByQuote,
+  getAllQuotes
 } from "./Services/quotesDatabase.js";
 class App extends Component {
-	render() {
-		let quote = getAllQuotes();
-		return (
-			<div className='container'>
-				<h1>List of Quotes</h1>
-				{quote.map((q) => (
-					<Quote
-						quote={q}
-						author={getInformationByQuote(q)[q].a}
-						tag={getInformationByQuote(q)[q].t}
-						category={getInformationByQuote(q)[q].c}
-					/>
-				))}
-			</div>
-		);
-	}
+  render() {
+    let quote = getAllQuotes();
+    return (
+      <div className="container">
+        <h1>List of Quotes</h1>
+        {quote.map(q => (
+          <Quote
+            quote={q}
+            author={getInformationByQuote(q)[q].a}
+            tag={getInformationByQuote(q)[q].t}
+            category={getInformationByQuote(q)[q].c}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
